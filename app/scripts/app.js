da1433811695f8212debc7f2c48aea55eb86a451
Javier Buquet <jbuquet@gmail.com>
2015-07-15 18:40:46 -0300

    Bootstrap app with yeoman angular-generator

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name officerankWebApp
+ * @description
+ * # officerankWebApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('officerankWebApp', [
+    'ngResource'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name officerankWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the officerankWebApp
+ */
+angular.module('officerankWebApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
